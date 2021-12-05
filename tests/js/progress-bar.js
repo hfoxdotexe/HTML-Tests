4a4437c10f005a66e20b3593326b4c4d9a54963e
execfox <81385231+hfoxdotexe@users.noreply.github.com>
2021-12-05 01:28:32 -0500

    Update progress-bar.js

--- tests/js/progress-bar.js
+++ tests/js/progress-bar.js
@@ -2,3 +2,7 @@ function setPercentage(id, percentage) {
 	var elm = document.getElementById(id);
 	elm.style.width = percentage + "%";
 }
+
+function changePercentage(id, percentage) {
+	setPercentage(document.getElementById(id) + percentage);
+}
