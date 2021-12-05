fedc3f33790118d5cc5bd426321623d0b90a4339
execfox <81385231+hfoxdotexe@users.noreply.github.com>
2021-12-05 01:59:35 -0500

    Update progress-bar.js

--- tests/js/progress-bar.js
+++ tests/js/progress-bar.js
@@ -1,4 +1,4 @@
-document.getElementById(id).style.width = 0;
+document.getElementById(id).style.width = '0%';
 
 function setPercentage(id, percentage) {
 	var elm = document.getElementById(id);
@@ -8,9 +8,6 @@ function setPercentage(id, percentage) {
 function changePercentage(id, percentage) {
 	var v = document.getElementById(id).style.width
 	v = parseInt(v)
-	if v == 'NaN' {
-		v = 0
-	}
 	var inc = v + percentage;
 	setPercentage(id, inc);
 }
